8bb02509398f7ed4b24bb576f0c60737379acafa
uijin_lee <134524937+uijindaramg@users.noreply.github.com>
2024-08-23 15:00:47 +0900

    Update app.py_ another branch

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, jsonify, request
-from ultrasonic import check_ultrasonic_sensor, cleanup
-from face_recognition import perform_face_registration
+from flask import Flask, render_template, request, jsonify
+import ultrasonic
+import face_insert
 
 app = Flask(__name__)
 
@@ -8,28 +8,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/insertuser')
-def insert_user():
+@app.route('/register_user', methods=['GET'])
+def register_user():
     return render_template('insertuser.html')
 
-@app.route('/start-registration', methods=['POST'])
-def start_registration():
-    user_detected = check_ultrasonic_sensor()
-    if user_detected:
-        return jsonify({"status": "user_detected", "message": "얼굴인식을 시작합니다"})
+@app.route('/start_ultrasonic', methods=['POST'])
+def start_ultrasonic():
+    user_present = ultrasonic.check_ultrasonic_sensor()
+    
+    if user_present:
+        return jsonify({"status": "success", "message": "User detected. Starting face recognition..."})
     else:
-        return jsonify({"status": "user_not_detected", "message": "사용자가 감지되지 않았습니다"})
+        return jsonify({"status": "fail", "message": "User not detected. Please stand closer to the sensor."})
 
-@app.route('/perform-face-recognition', methods=['POST'])
-def perform_face_recognition():
-    registration_successful = perform_face_registration()
-    if registration_successful:
-        return jsonify({"status": "success", "message": "정상등록 되었습니다"})
-    else:
-        return jsonify({"status": "failure", "message": "얼굴 등록에 실패했습니다"})
+@app.route('/start_face_registration', methods=['POST'])
+def start_face_registration():
+    user_id = request.form.get('userID')
+    face_insert.register_face(user_id)
+    return jsonify({"status": "success", "message": "Face registered successfully!"})
 
-if __name__ == "__main__":
-    try:
-        app.run(host='0.0.0.0', port=5000)
-    finally:
-        cleanup()  # GPIO 리소스 해제
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
