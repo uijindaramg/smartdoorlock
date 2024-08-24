cbce9d2b36cf4485abe776896a527622867aaadb
seoseunga <aseo1014@ewha.ac.kr>
2024-08-24 19:48:07 +0900

    Update led_buzzer.py

--- test/password/led_buzzer.py
+++ test/password/led_buzzer.py
@@ -2,16 +2,16 @@ import RPi.GPIO as GPIO
 import time
 
 # 핀 번호 설정
-RED_PIN = 17    
-GREEN_PIN = 27 
-YELLOW_PIN = 25
+RED_LED = 17    
+GREEN_LED = 27 
+YELLOW_LED = 25
 BUZZER_PIN = 4  
 
 # GPIO 모드 설정
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(RED_LED_PIN, GPIO.OUT)
-GPIO.setup(GREEN_LED_PIN, GPIO.OUT)
-GPIO.setup(YELLOW_LED_PIN, GPIO.OUT)
+GPIO.setup(RED_LED, GPIO.OUT)
+GPIO.setup(GREEN_LED, GPIO.OUT)
+GPIO.setup(YELLOW_LED, GPIO.OUT)
 GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
 scale = [523,  554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1046]
@@ -33,7 +33,7 @@ def led_off(color):
     GPIO.output(GREEN_LED, False)
 
 def Buzzer_CEGC():
-  buzzer = GPIO.PWM(buzzer_pin, 1)
+  buzzer = GPIO.PWM(BUZZER_PIN, 1)
   buzzer.start(50)
   buzzer.ChangeFrequency(scale[0])
   time.sleep(0.5)
@@ -48,14 +48,14 @@ def Buzzer_CEGC():
   GPIO.cleanup()
 
 def Buzzer_BEEP():
-  buzzer = GPIO.PWM(buzzer_pin, 1000)
+  buzzer = GPIO.PWM(BUZZER_PIN, 1000)
   buzzer.start(50)
   time.sleep(1)
   buzzer.stop()
   GPIO.cleanup()
 
 def Buzzer_ROOF():
-  buzzer = GPIO.PWM(buzzer_pin, 1000)
+  buzzer = GPIO.PWM(BUZZER_PIN, 1000)
   while True:
     buzzer.start(50)
     time.sleep(0.5)
@@ -63,7 +63,7 @@ def Buzzer_ROOF():
     time.sleep(0.5)
 
 def Buzzer_OFF():
-  buzzer = GPIO.PWM(buzzer_pin, 1)
+  buzzer = GPIO.PWM(BUZZER_PIN, 1)
   buzzer.ChangeDutyCycle(0)
   buzzer.stop()
   GPIO.cleanup()
